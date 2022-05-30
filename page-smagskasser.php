bebda7e3b62fd420495ff92ec7d5c68ea1d287ea
Regitze Reimer Rønneberg <regi5539@stud.kea.dk>
2022-05-30 11:14:37 +0200

    rettelse i nogle af  const til rigtige navne

--- page-smagskasser.php
+++ page-smagskasser.php
@@ -44,6 +44,53 @@ get_header(); ?>
         </div><!-- end wrapper -->
     </main><!-- end main -->
 
+
+
+<script>
+    console.log("detvirker")
+    console.log("LoadSite")
+	 
+	const url = "https://freyaluntang.dk/kea/eksamensprojekt/beerlivery_wp/wp-json/wp/v2/smagskasse?per_page=100";
+    
+    // denne skal bruges hvis man bruger kategorier
+    const caturl = "https://freyaluntang.dk/kea/eksamensprojekt/beerlivery_wp/wp-json/wp/v2/categories";
+	
+	
+	let smagskasser;
+
+    hentData(url);
+
+	async function hentData() {
+        const respons = await fetch(url);
+        smagskasser = await respons.json();
+        visSmagskasser();
+      }
+
+	    function visSmagskasser() {
+			console.log({ smagskasser });
+
+			const container = document.querySelector("#container");
+       		const template = document.querySelector("template");
+
+            // her sletter den det tekst der kunne stå i forvejen (kun brugt når der er filtrering)   
+			container.textContent = "";
+
+			smagskasser.forEach((smagskasse) => {
+            	const klon = template.cloneNode(true).content;
+            	klon.querySelector(".billede").src = ret.billede.guid;
+           		klon.querySelector(".titel").textContent = ret.title.rendered;
+            	klon.querySelector(".info").textContent = ret.kort_beskrivelse;
+            	klon.querySelector(".pris").textContent = ret.pris + " kr.";
+            	klon
+              		.querySelector("article")
+              		.addEventListener("click", () => { location.href = ret.link; })
+            	container.appendChild(klon);
+        });
+      }
+		
+</script>
+
+
 <?php get_sidebar('footer'); ?>
 <?php get_footer(); ?>
 
